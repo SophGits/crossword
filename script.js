38d1768ac79b9aff3732ebb4a4cc9fa48ae88a3b
SophGits <sophie.jiffy@gmail.com>
2014-11-17 23:45:49 +0000

    Create models, collections and views.

--- script.js
+++ script.js
@@ -18,5 +18,64 @@ game.Board = Backbone.Collection.extend({ // a collection of squares
   model: game.Square,
   localStorage: new Store("crossword")
 });
+game.board = new game.Board(); // initialise collection
 
-game.board = new game.Board();
+// Views
+
+// Render individual Square view
+game.SquareView = Backbone.View.extend({
+  className: 'square',
+  template: _.template($('#board-template').html()),
+  render: function(){
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  },
+  initialize: function(){
+    this.model.on('change', this.render, this);
+    this.model.on('destroy', this.remove, this);
+  },
+  events: {
+    'keyup .edit' : 'update',
+    'click .remove' : 'destroy'
+  },
+  update: function(){
+    this.input = this.$('.edit');
+    var value = this.input.val();
+    this.model.save({letter: value});
+  },
+  destroy: function(){
+    this.model.destroy();
+  }
+});
+// var view = new game.SquareView({model: game.Square});
+
+// Render whole Board view
+game.BoardView = Backbone.View.extend({
+  el: '#crosswordapp',
+  initialize: function(){
+    game.board.on('add', this.addSquare, this);
+    game.board.fetch();
+  },
+  events: {
+    'click #board': 'createSquare'
+  },
+  createSquare: function(){
+    game.board.create(this.newAttributes());
+  },
+  addSquare: function(square){
+    var view = new game.SquareView({model: square});
+    $('#board').append(view.render().el);
+  },
+  newAttributes: function(){
+    console.log(this);
+    if(this.input){
+      return{letter: this.input.val()}
+    } else {
+      return{letter: "^_^"}
+    }
+  }
+});
+
+// Initialisers
+Backbone.history.start();
+game.boardView = new game.BoardView();
